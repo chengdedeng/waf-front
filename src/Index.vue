c7881a0e56d5e29b309600bf3122e908a0e0ec71
yangguo <yangguo@outlook.com>
2017-12-27 10:45:44 +0800

    remove content header in content of index

--- src/Index.vue
+++ src/Index.vue
@@ -5,16 +5,7 @@
       <va-sidebar :slideMenuItems="slideMenuItems"></va-sidebar>
       <div class="content-wrapper">
         <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Dashboard
-            <small>Version 2.0</small>
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-          </ol>
-        </section>
+        <section class="content-header"></section>
         <section class="content">
           <transition name="page" mode="out-in">
             <router-view></router-view>
